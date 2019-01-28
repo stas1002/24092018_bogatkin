ad2bc19bd5fc5b463d42fed09aa559deddedaaa5
stas1002 <stas-bogatkin@ya.ru>
2019-01-28 19:26:13 +0300

    Добавлена структура проекта

--- index.php
+++ index.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Проект №3</title>
+    <link rel="stylesheet" href="/style/style.css">
 </head>
 <body>
     <h1>Код проекта №3</h1>
+
+    <script src="/js/main.js"></script>
 </body>
 </html>
